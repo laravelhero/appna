8c1f20d5ca298d2ccfb3542a5fa03de8f5e79201
Mohammad Shafayet Haydar <shafayetbs@gmail.com>
2024-06-05 00:52:25 +0600

    changes: payments view

--- payments/views.py
+++ payments/views.py
@@ -9,6 +9,7 @@ import uuid
 from app.forms import DonationForm, MemberForm
 from payments.forms import MemberPayPalPaymentsForm, DonationPayPalPaymentsForm
 
+
 def checkout(request):
     if request.method == 'POST':
         form_type = request.POST.get('form_type')
@@ -22,48 +23,96 @@ def checkout(request):
             form = MemberForm(request.POST, request.FILES)
             item_name = 'Membership'
 
-
         paypal_dict = {
-                "business": "sb-oso3c31035489@business.example.com",
-                "amount": amount,
-                "currency_code": 'USD',
-                "item_name": item_name,
-                "invoice": f'{uuid.uuid4()}-{uuid.uuid4()}',
-                "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
-                "return": request.build_absolute_uri(reverse('payments:payment_success')),
-                "cancel_return": request.build_absolute_uri(reverse('payments:payment_fail')),
+            "business": "sb-oso3c31035489@business.example.com",
+            "amount": amount,
+            "currency_code": 'USD',
+            "item_name": item_name,
+            "invoice": f'{uuid.uuid4()}-{uuid.uuid4()}',
+            "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
+            "return": request.build_absolute_uri(reverse('payments:payment_success')),
+            "cancel_return": request.build_absolute_uri(reverse('payments:payment_fail')),
         }
 
         if form.is_valid():
-            request.session['form'] = form.cleaned_data
+            request.session['form'] = form
             request.session['form_type'] = form_type
 
+            if form_type == 'donation':
+                paypal_form = DonationPayPalPaymentsForm(initial=paypal_dict)
+            else:
+                paypal_form = MemberPayPalPaymentsForm(initial=paypal_dict)
+
+            context = {
+                "paypal_form": paypal_form,
+                "amount": amount,
+                "form": form,
+                "form_type": form_type,
+            }
+
+            return render(request, 'payments/checkout.html', context)
+
 
         if form_type == 'donation':
-            paypal_form = DonationPayPalPaymentsForm(initial=paypal_dict)
+            template = 'app/donation.html'
         else:
-            paypal_form = MemberPayPalPaymentsForm(initial=paypal_dict)
+            template = 'app/member.html'
 
-        context = {
-            "paypal_form": paypal_form,
-            "amount": amount,
-            "form": form,
-            "form_type": form_type,
-        }
+        return render(request, template, {'form': form, 'form_type': form_type})
 
-        return render(request, 'payments/checkout.html', context)
+    return render(request, 'app/checkout.html')
 
-    else:
-        form_type = request.session.get('form_type', 'donation')
+
+# def checkout(request):
+#     if request.method == 'POST':
+#         form_type = request.POST.get('form_type')
+
         
-        if form_type == 'donation':
-            form = DonationForm()
-            template = 'donation.html'
-        else:
-            form = MemberForm()
-            template = 'member.html'
 
-        return render(request, template, {'form': form})
+#         if form_type == 'donation':
+#             amount = request.POST.get('amount')
+#             form = DonationForm(request.POST)
+#             item_name = 'Donation'
+#         else:
+#             amount = request.POST.get('membership_type')
+#             form = MemberForm(request.POST, request.FILES)
+#             item_name = 'Membership'
+
+
+#         paypal_dict = {
+#                 "business": "sb-oso3c31035489@business.example.com",
+#                 "amount": amount,
+#                 "currency_code": 'USD',
+#                 "item_name": item_name,
+#                 "invoice": f'{uuid.uuid4()}-{uuid.uuid4()}',
+#                 "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
+#                 "return": request.build_absolute_uri(reverse('payments:payment_success')),
+#                 "cancel_return": request.build_absolute_uri(reverse('payments:payment_fail')),
+#         }
+
+#         if form.is_valid():
+#             request.session['form'] = form
+#             request.session['form_type'] = form_type
+
+
+#             if form_type == 'donation':
+#                 paypal_form = DonationPayPalPaymentsForm(initial=paypal_dict)
+#             else:
+#                 paypal_form = MemberPayPalPaymentsForm(initial=paypal_dict)
+
+#             context = {
+#                 "paypal_form": paypal_form,
+#                 "amount": amount,
+#                 "form": form,
+#                 "form_type": form_type,
+#             }
+
+#             return render(request, 'payments/checkout.html', context)
+
+    
+ 
+
+#     return render(request, template, {'form': form})
 
 
 def payment_success(request):
