d3015bd4e0884bbfb45df6a68e647ecfd0d2691d
Mohammad Shafayet Haydar <shafayetbs@gmail.com>
2024-06-05 06:16:30 +0600

    fix: member form and donation form

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
@@ -18,8 +18,9 @@ def checkout(request):
             amount = request.POST.get('amount')
             form = DonationForm(request.POST)
             item_name = 'Donation'
+
         else:
-            amount = request.POST.get('membership_type')
+            amount = request.POST.get('amount')
             form = MemberForm(request.POST, request.FILES)
             item_name = 'Membership'
 
@@ -33,24 +34,25 @@ def checkout(request):
             "return": request.build_absolute_uri(reverse('payments:payment_success')),
             "cancel_return": request.build_absolute_uri(reverse('payments:payment_fail')),
         }
-
+     
         if form.is_valid():
-            request.session['form_data'] = form.cleaned_data
+            request.session['form'] = form.cleaned_data
             request.session['form_type'] = form_type
 
-            if form_type == 'donation':
-                paypal_form = DonationPayPalPaymentsForm(initial=paypal_dict)
-            else:
-                paypal_form = MemberPayPalPaymentsForm(initial=paypal_dict)
+            # if form_type == 'donation':
+            #     paypal_form = DonationPayPalPaymentsForm(initial=paypal_dict)
+            # else:
+            #     paypal_form = MemberPayPalPaymentsForm(initial=paypal_dict)
 
-            context = {
-                "paypal_form": paypal_form,
-                "amount": amount,
-                "form": form,
-                "form_type": form_type,
-            }
+            # context = {
+            #     "paypal_form": paypal_form,
+            #     "amount": amount,
+            #     "form": form,
+            #     "form_type": form_type,
+            # }
 
-            return render(request, 'payments/checkout.html', context)
+            # return render(request, 'payments/checkout.html', context)
+            return redirect('payments:payment_success')
 
         if form_type == 'donation':
             template = 'app/donation.html'
@@ -63,13 +65,15 @@ def checkout(request):
 
 def payment_success(request):
     if request.method == 'GET' and 'form' in request.session:
-        if(request.session.get('form_type')=='donation'):
-            form = DonationForm(request.session.get('form'))
+        if(request.session['form_type']=='donation'):
+            form_data = request.session['form']
+            form = DonationForm(form_data)
             form.save()
-        else:
-            form = MemberForm(request.session.get('form'))
+        elif request.session['form_type']=='member':
+            form_data = request.session['form']
+            form = MemberForm(form_data)
     
-            User.objects.create_user(username='shafayet@gmail.com', password='testing321')
+            User.objects.create_user(username=form_data.get('email'), password=form_data.get('password'), is_staff=True, is_active=True)
             form.save()
 
         del request.session['form']
